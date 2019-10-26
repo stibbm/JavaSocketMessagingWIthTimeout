afb3a7e7ae50ac8b8a9a7fc5a61f5dff3d2c9abc
Matthew Stibbins <stibbm@u.washington.edu>
2019-10-26 06:28:06 -0700

    added logic to send message and some comments to outbound socket

--- OutboundSocket.java
+++ OutboundSocket.java
@@ -3,14 +3,43 @@ import java.io.*;
 import java.net.*;
 
 
-public class OutboundSocket {
+public class OutboundSocket implements Runnable {
+
+
+    static final int DEFAULT_BASE_TIMEOUT = 150;
     
     Socket socket;
     Message message;
-    String destIpAddress;
+    String remoteIpAddress;
+    int remotePort;
 
-    public OutboundSocket(Message message, String destIpAddress){
+    // OutboundSocket constructor
+    // @Param Message message
+    // --> Wrapper for object that is being sent
+    // @Param String remoteIpAddress
+    // --> Destination IP Address
+    // @Param int remotePort
+    // --> Destination port
+    public OutboundSocket(Message message, String remoteIpAddress, int remotePort){
         this.message = message;
-        this.destIpAddress = destIpAddress;
+        this.remoteIpAddress = remoteIpAddress;
+        this.remotePort = remotePort;
+        this.socket = new Socket(remoteIpAddress, remotePort);
+        this.socket.setSoTimeout(150);
     }
+    
+    // sends the message to the specified destination
+    @Override
+    public void run(){
+        try{
+            this.ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            oos.writeObject(this.message);
+            oos.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
