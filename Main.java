758a65153a702c567fd980024a124451949c82a9
Matthew Stibbins <stibbm@u.washington.edu>
2019-10-26 06:33:33 -0700

    added logic in main method to test sending and recieving messages through the sockets

--- Main.java
+++ Main.java
@@ -9,11 +9,19 @@ public class Main {
 		Queue<Message> inboundMessageQueue = new LinkedList<>();
 		Thread inboundSocket = new Thread(new InboundSocket(inboundMessageQueue));
 		inboundSocket.start();
-		
+
 		while(true){
 			try{
-				Thread.sleep(1000);
+				Thread.sleep(10000);
+				String messageText = "messageText";
+				Message message = new Message(messageText);
+				String localhost = "127.0.0.1";
+				int remotePort = 8987;
+				Thread outboundSocket = new Thread(
+					new OutboundSocket(message, localhost, remotePort));
+				outboundSocket.start();	
 			}catch(Exception e){
+				e.printStackTrace();
 			}
 			System.out.println(inboundMessageQueue);
 		}
