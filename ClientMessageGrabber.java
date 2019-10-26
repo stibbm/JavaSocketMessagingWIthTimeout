e8e81c30c84083a3f8815c801a7f2dcef7cd7c28
Matthew Stibbins <48364517+stibbm@users.noreply.github.com>
2019-10-25 21:36:05 -0700

    changed location of comment on ClientMessageGrabber from the run method to the constructor to explain the arguments to the constructor.

--- ClientMessageGrabber.java
+++ ClientMessageGrabber.java
@@ -4,19 +4,20 @@ class ClientMessageGrabber implements Runnable {
   Socket clientSocket;
 
 
-  // 
+  // constructor
+  // @Param Socket clientSocket
+  // --> The socket connection to the client we are accepting the
+  // message from
+  // @Param Queue<Message>
+  // --> Will place the message into the message queue
   public ClientMessageGrabber(Socket clientSocket,
     Queue<Message> inboundMessageQueue){
     this.inboundMessageQueue = inboundMessageQueue;
     this.clientSocket = clientSocket;
   }
 
-  // recieves the message from the client and stores it in the Queue
-  // @Param Socket clientSocket
-  // --> The socket connection to the client we are accepting the
-  // message from
-  // @Param Queue<Message>
-  // --> Will place the message into the message queue
+  // recieves the message from the client and stores it in the Queue which can
+  // be accessed by the creator of the thread
   @Override
   public void run(){
 
