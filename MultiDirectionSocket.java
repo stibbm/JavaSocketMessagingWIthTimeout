b5eac5eb86457293c466795c1ac0651ae3fbe009
Matthew Stibbins <stibbm@u.washington.edu>
2019-10-26 16:21:27 -0700

    added send and receive message logic to multidirectional socket

--- MultiDirectionSocket.java
+++ MultiDirectionSocket.java
@@ -2,18 +2,46 @@ import java.util.*;
 import java.io.*;
 import java.net.*;
 
-class MultiDirectionSocket{
+class MultiDirectionSocket {
 
     Queue<Message> messagesReceived;
     Thread inboundSocket;
     Thread outboundSocket;
+    String remoteIpAddress;
+    int remotePort;
+    int inboundPort;
 
-    
+    // immediately starts listening for incoming connections
     class MultiDirectionSocket(){
         messagesReceived = new LinkedList<>();
-        inboundSocket = new Thread(new InboundSocket());
-        
-
+        inboundSocket = new Thread(new InboundSocket(messagesReceived));
+        inboundSocket.start();
     }
+
+    public void sendObject(Object obj){
+        try{
+            Message message = new Message(obj);
+            Thread outboundSocket = new Thread(
+                new OutboundSocket(message, remoteIpAddress, remotePort));
+            outboundSocket.start(); 
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public Message readNextMessage(){
+        synchronized(messagesReceived){
+            if(messagesReceived.size()==0){
+                return null;
+            }
+            else{
+                Message message = (Message)messagesReceived.peek();
+                messagesReceived.pop();
+                return message;
+            }
+        }
+    }
+
+
 
 }
